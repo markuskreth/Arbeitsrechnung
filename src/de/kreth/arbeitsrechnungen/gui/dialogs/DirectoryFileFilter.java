6de9a828093619ef771fba90c0f78620299fc964
Markus <markus.kreth@web.de>
2013-09-25 06:19:38 +0200

    Optionen-Dialog benutzt stärker den JFilechooser statt awt, unfertig.

---
+++ src/de/kreth/arbeitsrechnungen/gui/dialogs/DirectoryFileFilter.java
@@ -0,0 +1,18 @@
+package de.kreth.arbeitsrechnungen.gui.dialogs;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class DirectoryFileFilter extends FileFilter {
+
+	@Override
+	public String getDescription() {
+		return "Nur Verzeichnisse";
+	}
+	
+	@Override
+	public boolean accept(File f) {
+		return f.isDirectory();
+	}
+}
