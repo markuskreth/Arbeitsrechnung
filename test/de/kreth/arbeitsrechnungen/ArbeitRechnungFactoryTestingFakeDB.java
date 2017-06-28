7a2200af759485fa8d46e06e2808b1df5f5d4b0c
Markus Kreth <markus.kreth@web.de>
2017-06-28 20:30:30 +0200

    Text fixture and mocking.

--- test/de/kreth/arbeitsrechnungen/ArbeitRechnungFactoryTestingFakeDB.java
+++ test/de/kreth/arbeitsrechnungen/ArbeitRechnungFactoryTestingFakeDB.java
@@ -7,20 +7,35 @@ import de.kreth.arbeitsrechnungen.test.MockKlientenEditorPersister;
 
 public class ArbeitRechnungFactoryTestingFakeDB extends ArbeitRechnungFactory {
 
+   public MockRechnungDialogPersister rechnungDialogPersister = null;
+   public KlientenEditorPersister klientenEditorPersister = null;
+   
+   
    private ArbeitRechnungFactoryTestingFakeDB() {
-
    }
 
-   public static void init() {
-      instance = new ArbeitRechnungFactoryTestingFakeDB();
+   public static ArbeitRechnungFactoryTestingFakeDB init() {
+      ArbeitRechnungFactoryTestingFakeDB arbeitRechnungFactoryTestingFakeDB = new ArbeitRechnungFactoryTestingFakeDB();
+      instance = arbeitRechnungFactoryTestingFakeDB;
+      return arbeitRechnungFactoryTestingFakeDB;
    }
 
    @Override
    public Persister getPersister(Class<? extends Persister> clazz, Options optionen) {
-      if (clazz == KlientenEditorPersister.class)
-         return new MockKlientenEditorPersister();
-      if (clazz == RechnungDialogPersister.class)
-         return new MockRechnungDialogPersister();
+      if (clazz == KlientenEditorPersister.class) {
+         if(klientenEditorPersister != null) {
+            return klientenEditorPersister;
+         } else {
+            return new MockKlientenEditorPersister();
+         }
+      }
+      if (clazz == RechnungDialogPersister.class) {
+         if(rechnungDialogPersister != null) {
+            return rechnungDialogPersister;
+         } else {
+            return new MockRechnungDialogPersister();
+         }
+      }
       throw new IllegalArgumentException("Klasse " + clazz.getSimpleName() + " nicht unterstützt...");
    }
 
